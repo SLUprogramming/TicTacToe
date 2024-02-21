b5be8365d1cbb9bb3d33a13e2c8c1df78fd5ccfc
SLUprogramming <141665230+SLUprogramming@users.noreply.github.com>
2024-02-20 18:25:09 -0600

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -12,6 +12,7 @@ Player 2 moves second and marks the o's
 ''')
 
 dictionary = {"1": ' ', "2": ' ', "3": ' ', "4": ' ', "5": ' ', "6": ' ', "7": ' ', "8": ' ', "9": ' '}
+# defines initial, empty board
 
 def printboard():
     board = f'''
@@ -24,19 +25,27 @@ This is current board:
 '''
     print(board)
 printboard()
+# retrieves values of dictionary then prints board
 
 non_numerals = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', ',', '<', '.', '>', '/', '?', '\\', '|', '~', '`']
+# List of invalid inputs
 
 alive = True
 used_non_numeral = False
 victory = False
 
 def checkwin():
+    '''
+    Defines all the sequences of 3 consecutive blocks on tic-tac-toe board
+    If it is found that all the elements in one of these sequences are equal
+    then the game is won
+    '''
     combos = [('1', '2', '3'), ('4', '5', '6'), ('7', '8', '9'),('1', '4', '7'), ('2', '5', '8'), ('3', '6', '9'),('1', '5', '9'), ('3', '5', '7')]
     for combo in combos:
         if dictionary.get(combo[0]) == dictionary.get(combo[1]) == dictionary.get(combo[2]) != ' ':
             return True
-    return False # careful on the indentation here!
+    return False
+
 
 def playagain():
     response = input("Would you like to play again (yes/no): ")
@@ -45,6 +54,7 @@ def playagain():
     else:
         print("Game over")
         return False
+# Allows users to play another game
 
 def clearboard():
     global dictionary
@@ -58,8 +68,12 @@ This is current board:
 {dictionary.get('7')}|{dictionary.get('8')}|{dictionary.get('9')}
 '''
     print(board)
+# Resets the board if the user chooses to play another game
 
 def gameloop(used_non_numeral, alive):
+    '''
+    main game loop
+    '''
     turns = 0
     while alive:
         choice = (input('Player 1 turn. For "X" please input a position from 1-9: '))
@@ -119,7 +133,6 @@ def gameloop(used_non_numeral, alive):
                     dictionary[choice2] = 'O'
                     turns += 1
                     printboard()
-                #checkwin() <---- this is not necessary
                 if checkwin():
                     print("Player 2 is the winner")
                     if playagain():
@@ -129,3 +142,4 @@ def gameloop(used_non_numeral, alive):
                     player2 = False
 
 gameloop(used_non_numeral, alive)
+# starts the game for the first time
